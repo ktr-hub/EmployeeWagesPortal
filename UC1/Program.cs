6ca3304923b01c3d9757d4f83f60c270413a6d17
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-27 09:20:17 +0530

    [Tirupathi] Add. UC1

---
+++ UC1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        Boolean attendanceCheck()
+        {
+            Random random = new Random();
+            int checker = random.Next(0, 2);
+            if (checker == 1)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Program attendanceObject = new Program();
+            if (attendanceObject.attendanceCheck())
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present");
+            }
+        }
+    }
+}
